5fd146e6c63de4976f52ccfd24ef0f8bd5c4df2c
gadget1999 <gadget1999@outlook.com>
2019-06-27 14:55:09 -0400

    Create get-cmds.sh

---
+++ get-cmds.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /tmp
+wget https://github.com/gadget1999/linux/archive/master.zip
+unzip master.zip
+sudo cp /tmp/linux-master/scripts/* /usr/local/bin/
+sudo chmod +x /usr/local/bin/*
+ls -l /usr/local/bin
+rm -R /tmp/linux-master
+rm /tmp/master.zip
