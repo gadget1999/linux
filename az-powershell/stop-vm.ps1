45cf5893ebcd4086fe1bb4ca6cd1ee9292544da5
gadget1999 <gadget1999@outlook.com>
2019-07-21 18:02:05 -0400

    Stop one VM by name

---
+++ az-powershell/stop-vm.ps1
@@ -0,0 +1,23 @@
+param (
+ [Parameter(Mandatory=$true)][string]$VMName
+)
+
+. "./logger.ps1"
+. "./find-vm.ps1"
+$logFile="/tmp/stop-vm.log"
+$logLevel="INFO"
+#$logLevel="DEBUG"
+
+$vm = Find-VM -VMName $VMName
+if ($vm.Name -ine $VMName) {
+ Return
+}
+
+if ($vm.PowerState -ne "VM running") {
+ Write-Log "VM not running: $VMName" "DEBUG"
+ Return
+}
+
+Write-Log "Shutting down VM: $VMName"
+Stop-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $VMName -Force
+Write-Log "Completed"
