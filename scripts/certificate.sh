c1a9e154818b669109c5969691b8a2a3d2be5716
gadget1999 <gadget1999@outlook.com>
2019-07-20 20:26:14 -0400

    Update certificate.sh

--- scripts/certificate.sh
+++ scripts/certificate.sh
@@ -7,29 +7,24 @@ source $CMD_PATH/common.sh
 # Acme.sh based wrapper to handle certificate issue and renew
 ##############################################################
 
-check_env "MAIN_USER CERT_ROOT CERT_METHOD CERT_LOCAL_PORT DDNS_DOMAIN"
+function check_cert_env() {
+ check_env "MAIN_USER CERT_ROOT CERT_LOCAL_PORT DDNS_DOMAIN"
 
-CERT_CMD=/home/$MAIN_USER/.acme.sh/acme.sh
+ CERT_CMD=/home/$MAIN_USER/.acme.sh/acme.sh
 
-CERT_FOLDER=$CERT_ROOT/$DDNS_DOMAIN
-if [ "$CERT_TYPE" == "ecc" ]; then
- CERT_FOLDER="$CERT_FOLDER"_ecc
-fi
+ CERT_FOLDER=$CERT_ROOT/$DDNS_DOMAIN
+ if [ "$CERT_TYPE" == "ecc" ]; then
+  CERT_FOLDER="$CERT_FOLDER"_ecc
+ fi
 
-CERT_FULLCHAIN="$CERT_FOLDER/fullchain.cer"
-CERT_KEY="$CERT_FOLDER/$DDNS_DOMAIN.key"
+ CERT_FULLCHAIN="$CERT_FOLDER/fullchain.cer"
+ CERT_KEY="$CERT_FOLDER/$DDNS_DOMAIN.key"
 
-case $CERT_METHOD in
- http)
-  CERT_TRANSPORT="--httpport $CERT_LOCAL_PORT"
-  ;;
- tls)
+ CERT_TRANSPORT="--httpport $CERT_LOCAL_PORT"
+ if [ "$CERT_METHOD" == "tls" ]; then
   CERT_TRANSPORT="--alpn --tlsport $CERT_LOCAL_PORT"
-  ;;
- *)
-  CERT_TRANSPORT=""
-  ;;
-esac
+ fi
+}
 
 function issue_certificate()  {
  local cmd="$SUDO $CERT_CMD --issue -d $DDNS_DOMAIN --standalone $CERT_TRANSPORT"
