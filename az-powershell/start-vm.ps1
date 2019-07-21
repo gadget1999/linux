ec76bf377ec73886fa93ea1872cd91f1fa9e0de2
gadget1999 <gadget1999@outlook.com>
2019-07-21 18:02:50 -0400

    Start one VM by name

---
+++ az-powershell/start-vm.ps1
@@ -0,0 +1,23 @@
+param (
+ [Parameter(Mandatory=$true)][string]$VMName
+)
+
+. "./logger.ps1"
+. "./find-vm.ps1"
+$logFile="/tmp/stop-vm.log"
+$logLevel="INFO"
+#$logLevel="DEBUG"
+
+$vm = Find-VM -VMName $VMName
+if ($vm.Name -ine $VMName) {
+ Return
+}
+
+if ($vm.PowerState -eq "VM running") {
+ Write-Log "VM already running: $VMName" "DEBUG"
+ Return
+}
+
+Write-Log "Starting VM: $VMName"
+Start-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $VMName
+Write-Log "Completed"
