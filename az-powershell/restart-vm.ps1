5f3dd6957401f3cb2d0cb0c21413039619fcb345
gadget1999 <gadget1999@outlook.com>
2019-07-22 18:01:23 -0400

    Don't restart if not running

--- az-powershell/restart-vm.ps1
+++ az-powershell/restart-vm.ps1
@@ -15,6 +15,11 @@ if ($vm.Name -ine $VMName) {
  Return
 }
 
+if ($vm.PowerState -ne "VM running") {
+ Write-Log "VM not running: $VMName" "DEBUG"
+ Return
+}
+
 Write-Log "Shutting down VM: $VMName"
 Stop-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $VMName -Force
 
