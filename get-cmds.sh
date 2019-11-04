e9d0fbccbdca3c6c22a52fab10a6cb4ee63cf298
gadget1999 <gadget1999@outlook.com>
2019-11-04 13:58:18 -0500

    copy lib folder

--- get-cmds.sh
+++ get-cmds.sh
@@ -7,6 +7,8 @@ wget https://github.com/gadget1999/linux/archive/master.zip
 unzip master.zip
 sudo cp /tmp/linux-master/scripts/* /usr/local/bin/
 sudo chmod +x /usr/local/bin/*
+sudo mkdir /usr/local/bin/lib
+sudo cp /tmp/linux-master/scripts/lib/* /usr/local/bin/lib/
 ls -l /usr/local/bin
 rm -R /tmp/linux-master
 rm /tmp/master.zip
