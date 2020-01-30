fcfc2b56a1883073f292922bc0fcbc7fa89fa8dc
gadget1999 <debian08807@gmail.com>
2020-01-30 10:26:51 -0500

    Azure REST API with python

--- scripts/python/azure-vm.py
+++ scripts/python/azure-vm.py
@@ -1,80 +1,168 @@
 import os
 import re
+import time
+import json
+import requests
 
-from azure.common.credentials import ServicePrincipalCredentials
-from azure.mgmt.compute import ComputeManagementClient
-from azure.mgmt.subscription import SubscriptionClient
-from msrestazure.azure_exceptions import CloudError
+class AzureSubscription:
+  def __init__(self, subscription_id, subscription_name):
+    self.Name = subscription_name
+    self.Id = subscription_id
 
+VM_STATUS_DEALLOCATED = "PowerState/deallocated"
+VM_STATUS_DEALLOCATING = "PowerState/deallocating"
+VM_STATUS_RUNNING = "PowerState/running"
+VM_STATUS_STARTING = "PowerState/starting"
 class AzureVM:
-  def __init__(self, subscription_id, rg_name, vm_name):
-    self.SubscriptionId = subscription_id
-    self.ResourceGroupName = rg_name
-    self.Name = vm_name
+  def __init__(self, cli, vm_json):
+    self._azure_cli = cli
+    vm_id = vm_json["id"]
+    # parse resource group from id: e.g., /subscriptions/<GUID>/resourceGroups/
+    self.subscription_id = re.search('\/subscriptions\/(.+?)\/resourceGroups\/', vm_id).group(1)
+    # parse resource group from id: e.g., /resourceGroups/<NAME>/providers/
+    self.resource_group_name = re.search('\/resourceGroups\/(.+?)\/providers\/', vm_id).group(1)
+    self.name = vm_json["name"]
+    self.size = vm_json['properties']['hardwareProfile']['vmSize']
+    self.os = vm_json['properties']['storageProfile']['osDisk']['osType']
 
+  def GetStatus(self):
+    API_URL = f"{AZURE_API_ENDPOINT}/subscriptions/{self.subscription_id}/resourceGroups/{self.resource_group_name}/providers/Microsoft.Compute/virtualMachines/{self.name}/instanceView?api-version=2019-07-01"
+    response = self._azure_cli.API_call(API_URL)
+    return response['statuses'][-1]['code']
+
+  def Start(self):
+    status = self.GetStatus()
+    if (status in {VM_STATUS_RUNNING, VM_STATUS_STARTING}):
+      print(f"Skipping... (VM status: {status})")
+      return
+
+    print(f"Starting VM [{self.name}]...")
+    API_URL = f"{AZURE_API_ENDPOINT}/subscriptions/{self.subscription_id}/resourceGroups/{self.resource_group_name}/providers/Microsoft.Compute/virtualMachines/{self.name}/start?api-version=2019-07-01"
+    self._azure_cli.API_call_post(API_URL)
+
+    # check if operation completed
+    for _ in range(30):
+      if (self.GetStatus() == VM_STATUS_RUNNING):
+        print("Completed.")
+        break
+      time.sleep(10)
+    else:
+      print("Operation timed out")
+
+  def Stop(self):
+    status = self.GetStatus()
+    if (status in {VM_STATUS_DEALLOCATED, VM_STATUS_DEALLOCATING}):
+      print(f"Skipping... (VM status: {status})")
+      return
+
+    print(f"Stopping VM [{self.name}]...")
+    API_URL = f"{AZURE_API_ENDPOINT}/subscriptions/{self.subscription_id}/resourceGroups/{self.resource_group_name}/providers/Microsoft.Compute/virtualMachines/{self.name}/deallocate?api-version=2019-07-01"
+    self._azure_cli.API_call_post(API_URL)
+
+    # check if operation completed
+    for _ in range(30):
+      if (self.GetStatus() == VM_STATUS_DEALLOCATED):
+        print("Completed.")
+        break
+      time.sleep(10)
+    else:
+      print("Operation timed out")
+
+  def Restart(self):
+    self.Stop()
+    self.Start()
+
+  def ShowSummary(self):
+    print(f"""  VM: {vm.name}
+   Subscription: {vm.subscription_id}
+   Resource Group: {vm.resource_group_name}
+   Size: {vm.size}
+   OS: {vm.os}
+      """)
+
+AZURE_LOGIN_ENDPOINT = "https://login.microsoftonline.com"
+AZURE_API_ENDPOINT = "https://management.azure.com"
 class AzureCLI:
-  _computeClients = dict()
-  VMs = dict()
+  _access_token = None
+  _subscriptions = []
+  _virtual_machines = []
 
+  ########################################
   # Private methods
+  ########################################
 
   def __init__(self, tenant_id, app_id, app_key):
     self.TenantId = tenant_id
-    self._credentials = ServicePrincipalCredentials(client_id = app_id,
-                                       secret = app_key,
-                                       tenant = tenant_id)
-    self.__load_all_vms()
+    self.__login(tenant_id, app_id, app_key)
 
-  def __get_subscriptions(self):
-    subscription_client = SubscriptionClient(self._credentials)
-    return subscription_client.subscriptions.list()
+  def __login(self, tenant_id, app_id, app_key):
+    API_URL = f"{AZURE_LOGIN_ENDPOINT}/{tenant_id}/oauth2/token"
+    params = {
+        "grant_type": "client_credentials",
+        "client_id": app_id,
+        "client_secret": app_key,
+        "resource": AZURE_API_ENDPOINT
+      }
+    response = self.API_call(API_URL, data=params)
+    self._access_token = response['access_token']
 
-  def __load_all_vms(self):
-    subs = self.__get_subscriptions()
-    for sub in subs:
-      subscription_id = sub.subscription_id
-      self._computeClients[subscription_id] = ComputeManagementClient(self._credentials, subscription_id)
-      self.__load_vms(subscription_id)
+  def __load_subscriptions(self):
+    API_URL = f"{AZURE_API_ENDPOINT}/subscriptions?api-version=2019-11-01"
+    response = self.API_call(API_URL)
+    for subscription in response["value"]:
+      subscription_id = subscription["subscriptionId"]
+      subscription_name = subscription["displayName"]
+      self._subscriptions.append(AzureSubscription(subscription_id, subscription_name))
 
-  def __load_vms(self, subscription_id):
-    if subscription_id not in self._computeClients:
-      self._computeClients[subscription_id] = ComputeManagementClient(self._credentials, subscription_id)
-    vms = self._computeClients[subscription_id].virtual_machines.list_all()
-    for vm in vms:
-      # parse resource group from id: e.g., /resourceGroups/dev2051046045001/providers/
-      rgName = re.search('\/resourceGroups\/(.+?)\/providers\/', vm.id).group(1)
-      vmName = vm.name
-      self.VMs[vmName] = AzureVM(subscription_id, rgName, vmName)
+  def __load_virtual_machines(self, subscription_id):
+    API_URL = f"{AZURE_API_ENDPOINT}/subscriptions/{subscription_id}/providers/Microsoft.Compute/virtualMachines?api-version=2019-07-01"
+    response = self.API_call(API_URL)
+    for vm in response["value"]:
+      self._virtual_machines.append(AzureVM(self, vm))
 
+  ########################################
   # Public methods
+  ########################################
 
-  def stop_vm(self, vm_name):
-    if vm_name not in self.VMs:
-      print("VM [{}] not found.".format(vm_name))
-      return
-    vm = self.VMs[vm_name]
-    compute_client = self._computeClients[vm.SubscriptionId]
-    deallocate = compute_client.virtual_machines.deallocate(vm.ResourceGroupName, vm.Name)
-    print("Stopping VM '{}' ...".format(vm_name))
-    deallocate.wait()
+  @property
+  def subscriptions(self):
+    if not self._subscriptions:
+      self.__load_subscriptions()
+    return self._subscriptions
 
-  def start_vm(self, vm_name):
-    if vm_name not in self.VMs:
-      print("VM '{}' not found.".format(vm_name))
-      return
-    vm = self.VMs[vm_name]
-    compute_client = self._computeClients[vm.SubscriptionId]
-    start = compute_client.virtual_machines.start(vm.ResourceGroupName, vm.Name)
-    print("Starting VM '{}' ...".format(vm_name))
-    start.wait()
+  @property
+  def virtual_machines(self):
+    if not self._virtual_machines:
+      for subscription in self.subscriptions:
+        self.__load_virtual_machines(subscription.Id)
+    return self._virtual_machines
 
-  def restart_vm(self, vm_name):
-    self.stop_vm(vm_name)
-    self.start_vm(vm_name)
+  def API_call(self, url, data=None, headers=None):
+    if (self._access_token is not None):
+      headers = { "Authorization": f"Bearer {self._access_token}" }
+    response = requests.get(url, data=data, headers=headers, timeout=60)
+    assert (response is not None), "API result is empty."
+    assert (response.status_code < 400), "API returned error: {response.status_code}"
+    content_type = response.headers['Content-Type']
+    assert ('application/json' in content_type), "API result is not in JSON formatreturned error: {content_type}"
+    return response.json()
+
+  def API_call_post(self, url, data=None, headers=None):
+    if (self._access_token is not None):
+      headers = { "Authorization": f"Bearer {self._access_token}" }
+    response = requests.post(url, data=data, headers=headers, timeout=60)
+    assert (response is not None), "API result is empty."
+    assert (response.status_code < 400), "API returned error: {response.status_code}"
 
 if __name__ == "__main__":
   TENANT = os.environ['AZURE_TENANT_ID']
-  APP_ID = os.environ['AZURE_CLIENT_ID']
-  APP_KEY = os.environ['AZURE_CLIENT_SECRET']
+  APP_ID = os.environ['AZURE_APP_ID']
+  APP_KEY = os.environ['AZURE_APP_KEY']
   vm_cli = AzureCLI(TENANT, APP_ID, APP_KEY)
-  vm_cli.restart_vm('linux4us')
+  for vm in vm_cli.virtual_machines:
+    vm.ShowSummary()
+  
+  target_vm_name = "linux4hk"
+  target_vm = next(filter(lambda x: x.name == target_vm_name, vm_cli.virtual_machines))
+  if (target_vm is not None):
+    target_vm.Restart()
