9c94bdfd59884a403860cb9fea2f8c8d32fb0982
gadget1999 <gadget1999@outlook.com>
2020-05-03 13:30:23 -0400

    different logging level for console and file

--- scripts/python/common.py
+++ scripts/python/common.py
@@ -15,9 +15,10 @@ class Logger:
       try:
         fileHandler = logging.FileHandler(app_logfile)
         fileHandler.setFormatter(logging.Formatter("%(asctime)s: %(levelname)s - %(message)s"))
+        fileHandler.setLevel(logging.INFO)
         logger.addHandler(fileHandler)
       except Exception as e: 
         logger.error(f"Cannot open log file [{app_logfile}]: {e}")
     # set log level
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     return logger
