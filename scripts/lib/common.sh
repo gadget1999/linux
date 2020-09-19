527161433a3453410afb24c14e5b230a01f38db7
gadget1999 <gadget1999@outlook.com>
2020-09-18 20:23:26 -0400

    add Epoch

--- scripts/lib/common.sh
+++ scripts/lib/common.sh
@@ -5,6 +5,7 @@ CMD_PATH=$(dirname "$0")
 # Common variables
 NOW=$(date +"%Y_%m_%d-%H_%M_%S")
 TODAY=$(date +%y_%m_%d)
+EPOCH=$(date +%s)
 PROGRAM="${0##*/}"
 LOG=/tmp/$PROGRAM.log
 ENABLE_LOGGING=1
