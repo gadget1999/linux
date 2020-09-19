f007279f3ffc8eceebab4943d827c57921df9bb5
gadget1999 <gadget1999@outlook.com>
2020-09-19 08:48:46 -0400

    revert change

--- scripts/lib/firebase.sh
+++ scripts/lib/firebase.sh
@@ -4,25 +4,21 @@ CMD_PATH=$(dirname "$0")
 source $CMD_PATH/lib/common.sh
 
 function check_firebase_env {
- check_env "FB_BASE_URL FB_KEY"
+ check_env "FB_BASE_URL FB_KEY FB_ENDPOINT FB_AGENT"
  check_packages "curl jq"
- FIREBASE_API_ENDPOINT=$FB_BASE_URL
- FIREBASE_API_KEY=$FB_KEY
 }
 
 function firebase_setup() {
- FIREBASE_API_ENDPOINT=$1
- FIREBASE_API_KEY=$2
+ FB_BASE_URL=$1
+ FB_KEY=$2
 }
 
 function firebase_response() {
- check_env "FB_ENDPOINT FB_AGENT"
-
  local timestamp=$(date +"%H:%M:%S")
  local path=$1
  local state=$2
  local msg='{"status":"'"$state $NOW"'"}'
- local url="$FIREBASE_API_ENDPOINT/$FB_ENDPOINT/response/$FB_AGENT/$path.json?auth=$FIREBASE_API_KEY"
+ local url="$FB_BASE_URL/$FB_ENDPOINT/response/$FB_AGENT/$path.json?auth=$FB_KEY"
  local fullpath="/$path"
  
  debug "Sending firebase message to [$url]: $msg"
@@ -32,7 +28,7 @@ function firebase_response() {
 function firebase_send() {
  local path=$1
  local msg=$2
- local url="$FIREBASE_API_ENDPOINT/$path.json?auth=$FIREBASE_API_KEY"
+ local url="$FB_BASE_URL/$path.json?auth=$FB_KEY"
 
  debug "Sending firebase message to [$url]: $msg"
  curl -X PUT -d "$msg" "$url"
@@ -40,7 +36,7 @@ function firebase_send() {
 
 function firebase_get_json() {
  local path=$1
- local url="$FIREBASE_API_ENDPOINT/$path.json?auth=$FIREBASE_API_KEY"
+ local url="$FB_BASE_URL/$path.json?auth=$FB_KEY"
  curl -s "$url"
 }
 
@@ -78,10 +74,9 @@ function event_handler() {
 }
 
 function firebase_listen() {
- check_env "FB_ENDPOINT FB_AGENT"
-
- FB_REQUEST_URL="$FIREBASE_API_ENDPOINT/$FB_ENDPOINT/request.json?auth=$FIREBASE_API_KEY"
+ FB_REQUEST_URL="$FB_BASE_URL/$FB_ENDPOINT/request.json?auth=$FB_KEY"
  curl -s -N --http2 -H "Accept:text/event-stream" $FB_REQUEST_URL | \
+ # httpie is used to handle streaming events from Firebase
 # /usr/bin/http --stream "$FB_REQUEST_URL" Accept:'text/event-stream' | \
  while read -r line ; do
   [[ "$line" == *"data: {"* ]] && event_handler $line
@@ -97,8 +92,3 @@ function start_firebase_agent {
   sleep 10
  done
 }
-
-####################
-# Bootstraping
-####################
-check_firebase_env
