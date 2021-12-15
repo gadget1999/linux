48fcd3709676a680ad0c770360979f91fe6ede56
gadget1999 <gadget1999@outlook.com>
2021-12-14 21:13:38 -0500

    Fix SendGrid cURL issue

--- scripts/lib/email.sh
+++ scripts/lib/email.sh
@@ -15,17 +15,17 @@ function send_email() {
  local mailto=$1
  local subject=$2
  local body=$3
- 
- local maildata='{ \
-  "personalizations": \
-    [{"to":[{"email":"'${mailto}'"}]}], \
-    "from":{"email":"'${FROM_EMAIL}'","name":"'${FROM_NAME}'"}, \
-    "subject":"'${subject}'", \
-    "content":[{"type":"text/html","value":"'${body}'"}]}'
+
+ local maildata="{ \
+  \"personalizations\": \
+    [{\"to\":[{\"email\":\"$mailto\"}]}], \
+    \"from\":{\"email\":\"$FROM_EMAIL\",\"name\":\"$FROM_NAME\"}, \
+    \"subject\":\"$subject\", \
+    \"content\":[{\"type\":\"text/html\",\"value\":\"$body\"}]}"
 
  /usr/bin/curl --request POST \
   --url https://api.sendgrid.com/v3/mail/send \
-  --header 'Authorization: Bearer '$SENDGRID_API_KEY \
+  --header "Authorization: Bearer $SENDGRID_API_KEY" \
   --header 'Content-Type: application/json' \
-  --data "'$maildata'"
+  --data "$maildata"
 }
